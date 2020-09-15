ad99ec285fcd71a09f4db440b23e339118da92b8
Hunter Kantner <hunterkantner@gmail.com>
2020-09-14 21:08:46 -0700

    Built initial timer and button function

--- script.js
+++ script.js
@@ -3,16 +3,31 @@ questions = ["Commonly used data types DO NOT include:",
             "Arrays in JavaScript can be used to store _____.",
             "String values must be enclosed within _____ when being assigned to variables.",
             "A very useful tool used during development and debugging for printing content to the debugger is:"
-        ]
-answers0 = ["strings", "booleans", "alerts", "numbers"];
+        ]; //Array of questions 
+answers0 = ["strings", "booleans", "alerts", "numbers"]; // answers assigned to arrays with variables equal to # of question arrays
 answers1 = ["quotes", "curly brackets", "parenthesis", "square brackets"];
 answers2 = ["numbers and strings", "other arrays", "booleans", "all of the above"];
 answers3 = ["commas", "curly brackets", "quotes", "parenthesis"];
-answers4 = ["Javascript", "terminal / bash", "for loops", "console.log"]
+answers4 = ["Javascript", "terminal / bash", "for loops", "console.log"];
+timerDisplay = document.querySelector("#timer");
+timerDown = 60;
+
+document.getElementById("startbtn").addEventListener("click", beginQuiz); //begin quiz on start button click
 
 function beginQuiz() {
     var mainHide = document.getElementById("main")
-    mainHide.style.display = "block";
+    mainHide.style.display = "none";
+    startTimer();
 }
 
-document.getElementById("startbtn").addEventListener("click", beginQuiz(event));
+function startTimer() {
+    timerInterval = setInterval(function() {
+    timerDown--;
+    timerDisplay.textContent = timerDown + " seconds remaining.";
+    if(timerDown === 0) {
+        console.log("Timer is Over")
+        clearInterval(timerInterval);
+        //enter intials for score function here
+    };
+    }, 1000);
+}
